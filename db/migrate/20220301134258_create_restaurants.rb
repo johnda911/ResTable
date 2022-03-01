2cacff3835034bcea1e3774600b3d9e598c784ce
Katie Hang <katiehang1991@gmail.com>
2022-03-01 09:18:01 -0500

    feature branch testing

--- db/migrate/20220301134258_create_restaurants.rb
+++ db/migrate/20220301134258_create_restaurants.rb
@@ -1,8 +1,21 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
-
+      t.string :name, null: false
+      t.string :address, null: false
+      t.text :description, null: false, limit: 280
+      t.string :phone, null: false
+      t.string :cuisine, null: false
+      t.integer :expense
+      t.string :neighborhood
+      t.string :operation_hours
+      t.string :dining_style
+      t.string :dress_code
+      t.string :parking_details
+      t.string :payment_options
       t.timestamps
     end
+
+    add_index :restaurants, :name
   end
 end
