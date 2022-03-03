6dba2bc127ab5e8903f54f864f8f2d2ebd3e4f87
Katie Hang <katiehang1991@gmail.com>
2022-03-03 17:30:53 -0500

    start working on restaurant show

--- frontend/components/restaurants/show/restaurant_show_container.jsx
+++ frontend/components/restaurants/show/restaurant_show_container.jsx
@@ -2,11 +2,11 @@ import { connect } from "react-redux";
 import RestaurantShow from "./restaurant_show";
 import { requestRestaurant } from "../../../actions/restaurant_actions";
 
-// const mSTP = (state) => {
-//   return {
-//     restaurants: Object.values(state.entities.restaurants),
-//   };
-// };
+const mSTP = (state) => {
+  return {
+    restaurants: Object.values(state.entities.restaurants),
+  };
+};
 
 const mDTP = (dispatch) => {
   return {
