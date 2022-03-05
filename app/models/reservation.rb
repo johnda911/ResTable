f422c432cb9e47f488f07a1a1019941c29970050
Katie Hang <katiehang1991@gmail.com>
2022-03-05 16:02:56 -0500

    create model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,11 @@
 class Reservation < ApplicationRecord
+    validates :party_size, :date, :time, :phone, presence: true
+
+    # to be commented in after creatingn comments model
+    # has_many :comments
+    # has_many :commemters,
+    #     through: :comments,
+    #     source: :user
+   
+
 end
