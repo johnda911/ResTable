2caea8ce4b812f365300672081e40fed598c0332
Katie Hang <katiehang1991@gmail.com>
2022-03-31 18:56:53 -0400

    add animation effect when hover on a restaurant image

--- frontend/components/restaurants/index/restaurant_index_item.jsx
+++ frontend/components/restaurants/index/restaurant_index_item.jsx
@@ -5,17 +5,27 @@ import { BsCurrencyDollar } from "react-icons/bs";
 class RestaurantIndexItem extends React.Component {
   // handleClick() => {
   // }
+  restaurantName(name) {
+    return name.length < 30 ? (
+      <span className="res-profile-name">{name}</span>
+    ) : (
+      <span className="res-profile-name">{`${name.substring(0, 27)}...`}</span>
+    );
+  }
+
   render() {
     const { restaurant } = this.props;
     return (
       <Link
         to={`/restaurant/${restaurant.id}`}
-        // target="_blank"
         className="restaurant-profile-item"
       >
-        <img src={restaurant.restaurantPhotoURL} />
-        <span className="res-profile-img">{restaurant.photo}</span>
-        <span className="res-profile-name">{restaurant.name}</span>
+        <div className="res-profile-img">
+          <img src={restaurant.restaurantPhotoURL} />
+        </div>
+        {/* <div className="res-profile-img">{restaurant.photo}</div> */}
+        {/* <span className="res-profile-name">{restaurant.name}</span> */}
+        {this.restaurantName(restaurant.name)}
         <span className="res-profile-info">
           {restaurant.cuisine} · <ExpenseRange expense={restaurant.expense} /> ·{" "}
           {restaurant.neighborhood}
